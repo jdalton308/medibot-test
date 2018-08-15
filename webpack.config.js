602f2a9eb803aa5c34f78af796f46f76c3861648
Joe Dalton <joseph.dalton@charter.com>
2018-08-15 13:00:51 -0600

    Added production flags to npm scripts

--- webpack.config.js
+++ webpack.config.js
@@ -40,4 +40,12 @@ module.exports = {
       }
     ]
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ],
 };
